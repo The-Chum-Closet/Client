eb0bd97d23e7a32a5dd4cafb1539f3b6f86ab5b9
April Castaneda <dylanmcastaneda@gmail.com>
2023-09-20 12:54:43 -0500

    add basic 404 page

---
+++ client/src/features/misc/404/FourOhFour.jsx
@@ -0,0 +1,10 @@
+
+const FourOhFour = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Oops, This Page Doesn't Exist....</h1>
+    </div>
+  );
+};
+
+export default FourOhFour;
---
+++ client/src/features/misc/404/routes/index.jsx
@@ -0,0 +1,10 @@
+import FourOhFour from '../FourOhFour.jsx';
+
+const FourOhFourRoutes = [
+    {
+      path: '*',
+      element: <FourOhFour />,
+    },
+  ];
+
+export default FourOhFourRoutes;
